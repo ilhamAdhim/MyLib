56f0f5610affc17206a79204e07013423309fe0e
ilhamadhim <ilhamm179@gmail.com>
2021-10-16 00:34:55 +0700

    Added empty state, Change category onclick done

--- js/books.js
+++ js/books.js
@@ -36,6 +36,7 @@ const initializeBooks = () => {
                 id: +new Date(),
                 title: "Harry Potter and the Philosopher's Stone",
                 author: "J.K Rowling",
+                imageURL: "https://images.moviesanywhere.com/143cdb987186a1c8f94d4f18de211216/fdea56fa-2703-47c1-8da8-70fc5382e1ea.jpg?h=375&resize=fit&w=250",
                 year: 1997,
                 isComplete: false,
             },
@@ -43,6 +44,7 @@ const initializeBooks = () => {
                 id: +new Date() + 1,
                 title: "Harry Potter and the Philosopher's Stone",
                 author: "J.K Rowling",
+                imageURL: "https://images.moviesanywhere.com/143cdb987186a1c8f94d4f18de211216/fdea56fa-2703-47c1-8da8-70fc5382e1ea.jpg?h=375&resize=fit&w=250",
                 year: 1997,
                 isComplete: false,
             },
@@ -50,6 +52,7 @@ const initializeBooks = () => {
                 id: +new Date() + 2,
                 title: "Harry Potter and the Philosopher's Stone",
                 author: "J.K Rowling",
+                imageURL: "https://images.moviesanywhere.com/143cdb987186a1c8f94d4f18de211216/fdea56fa-2703-47c1-8da8-70fc5382e1ea.jpg?h=375&resize=fit&w=250",
                 year: 1997,
                 isComplete: false,
             },
@@ -57,6 +60,7 @@ const initializeBooks = () => {
                 id: +new Date() + 3,
                 title: "Harry Potter and Ilham Gokil",
                 author: "J.K Rowling",
+                imageURL: "https://images.moviesanywhere.com/143cdb987186a1c8f94d4f18de211216/fdea56fa-2703-47c1-8da8-70fc5382e1ea.jpg?h=375&resize=fit&w=250",
                 year: 2012,
                 isComplete: true,
             },
@@ -64,6 +68,7 @@ const initializeBooks = () => {
                 id: +new Date() + 4,
                 title: "Harry Potter and the Philosopher's Stone",
                 author: "Ilham Rowling",
+                imageURL: "https://images.moviesanywhere.com/143cdb987186a1c8f94d4f18de211216/fdea56fa-2703-47c1-8da8-70fc5382e1ea.jpg?h=375&resize=fit&w=250",
                 year: 2000,
                 isComplete: false,
             },
@@ -72,7 +77,6 @@ const initializeBooks = () => {
     } else
         loadDataFromStorage(STORAGE_KEY)
 }
-
 
 const addNewBook = ({ title, author, year, isComplete }) => {
     return {
@@ -92,13 +96,19 @@ const removeBookByID = bookID => {
     dataBooks = dataBooks.filter(item => item.id !== bookID);
     updateDataToStorage();
 }
-
-const updateBookByID = (updatedBook) => {
+// This function is used for changing from bookList -> Finished, and vice versa
+const updateBookByID = (bookID, category) => {
+    console.log(category)
     // Compare the id, if IDs are similar, then replace the object into the updated ones
     dataBooks.map(book =>
-        book.id === updatedBook.id ? updatedBook : x
+        book.id === bookID ?
+            book.isComplete = category === "bookList" ? true : false
+            :
+            book
     );
+    console.log(dataBooks)
     updateDataToStorage();
+    window.location.reload(false);
 }
 
 const getBookList = () => {
@@ -116,14 +126,12 @@ const getFinishedBookList = () => {
 const deleteAllBookList = () => {
     dataBooks = dataBooks.filter(item => item.isComplete === true)
     updateDataToStorage();
-    getBookList();
     window.location.reload(false)
 }
 
 const deleteAllFinishedBooks = () => {
     dataBooks = dataBooks.filter(item => item.isComplete === false)
     updateDataToStorage();
-    getFinishedBookList();
     window.location.reload(false)
 }
 
---
+++ js/dom.js
@@ -0,0 +1,76 @@
+const loadBookListToDOM = () => {
+    let bookListElement = document.getElementById("book_list")
+    let bookList = getBookList()
+    if (bookList.length > 0)
+        renderDataToDOM(bookListElement, bookList, "bookList")
+    else
+        renderEmptyDataToDOM(bookListElement, "Book List")
+}
+
+const loadBookFinishedToDOM = () => {
+    let finishedBookListElement = document.getElementById("finished_book_list")
+    let bookFinished = getFinishedBookList()
+    if (bookFinished.length > 0)
+        renderDataToDOM(finishedBookListElement, bookFinished, "bookFinished")
+    else
+        renderEmptyDataToDOM(finishedBookListElement, "Finished Book")
+}
+
+const renderEmptyDataToDOM = (element, category) => {
+    let emptySectionElement = document.createElement('div')
+
+    // Image
+    let imageElement = document.createElement('img')
+    imageElement.setAttribute('src', '../assets/undraw_Taking_notes_re_bnaf.svg')
+    imageElement.setAttribute('alt', "No data")
+    imageElement.classList.add('no_data_image')
+    emptySectionElement.append(imageElement)
+
+    let captionElement = document.createElement('div')
+    captionElement.classList.add('caption_empty')
+
+    captionElement.innerText = `No ${category} is found`
+    emptySectionElement.append(captionElement)
+
+    element.append(emptySectionElement)
+}
+
+const renderDataToDOM = (element, data, category) => {
+    data.forEach(book => {
+        let newCardElement = document.createElement('div')
+        newCardElement.classList.add("cardBook")
+
+        // Image
+        let imageElement = document.createElement('img')
+        imageElement.setAttribute('src', book.imageURL)
+        imageElement.setAttribute('alt', book.title)
+        imageElement.classList.add('book_image')
+        newCardElement.append(imageElement)
+
+        let descriptionElement = document.createElement('div')
+        descriptionElement.classList.add("book_description")
+        descriptionElement.innerHTML = `
+        <div class="book_title">
+            ${book.title}
+        </div>
+        <div class="book_year">
+            ${book.year}
+        </div>
+        <div class="book_actions">
+            <button class="btn_delete">
+            <i class="fa fa-trash-o" aria-hidden="true"></i>
+            </button>
+            <button class="btn_done" onclick='updateBookByID(${book.id}, "${category}")'>
+            ${category === "bookList" ? "✔" :
+                '<i class="fa fa-refresh" aria-hidden="true"></i>'
+            }
+            </button >
+        </div >
+    `
+        newCardElement.append(descriptionElement)
+        element.append(newCardElement)
+    });
+}
+
+loadBookListToDOM()
+loadBookFinishedToDOM()
